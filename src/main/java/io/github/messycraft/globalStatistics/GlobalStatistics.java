169f734b6d8524a1443b95b0516ec40420de1ccb
Mreate <1705482458@qq.com>
2026-04-01 02:09:20 +0800

    Initial Commit

---
+++ src/main/java/io/github/messycraft/globalStatistics/GlobalStatistics.java
@@ -0,0 +1,16 @@
+package io.github.messycraft.globalStatistics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlobalStatistics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
